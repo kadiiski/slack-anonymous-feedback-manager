0d66285b3152ae90b6b676286da2ad4636c05082
kadiiski <kadiiski94@gmail.com>
2024-11-25 16:16:39 +0200

    fixing vercell bug

--- utils/orm/sequelize.js
+++ utils/orm/sequelize.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+const pg = require("node:pg");
 
 // Use environment variables to configure the database
 const DB_TYPE = process.env.DB_TYPE || "sqlite"; // 'sqlite' or 'postgres'
@@ -10,6 +11,7 @@ if (DB_TYPE === "postgres") {
   // PostgresSQL configuration
   sequelize = new Sequelize(DATABASE_URL, {
     dialect: "postgres",
+    dialectModule: pg, // Use the 'pg' module for PostgresSQL (fixes error in Vercel)
     logging: false, // Disable logging or set to `console.log` for debugging
     dialectOptions: {
       ssl: {
